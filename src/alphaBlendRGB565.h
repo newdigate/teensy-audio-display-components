f10280027017cb2a00eb52b69e6e3be8165e678c
Newdigate <nicnewdigate@gmail.com>
2021-05-16 15:37:33 +0100

    correction

--- src/alphaBlendRGB565.h
+++ src/alphaBlendRGB565.h
@@ -3,6 +3,9 @@
 //
 #ifndef MULTIPLE_ALPHABLENDRGB565_H
 #define MULTIPLE_ALPHABLENDRGB565_H
+
+#include "Arduino.h"
+
 //copied from https://stackoverflow.com/a/50012418
 int16_t blendRGB565(uint32_t fg, uint32_t bg, uint8_t alpha );
 #endif //MULTIPLE_ALPHABLENDRGB565_H
