2121a89bbb54776beb1d3b7cd6eccff7cedf1b88
Newdigate <nicnewdigate@gmail.com>
2021-05-16 15:33:49 +0100

    keep separated

---
+++ src/alphaBlendRGB565.cpp
@@ -0,0 +1,11 @@
+#include "alphaBlendRGB565.h"
+
+//copied from https://stackoverflow.com/a/50012418
+int16_t blendRGB565(uint32_t fg, uint32_t bg, uint8_t alpha )
+{
+    alpha = ( alpha + 4 ) >> 3; // from 0-255 to 0-31
+    bg = (bg | (bg << 16)) & 0b00000111111000001111100000011111;
+    fg = (fg | (fg << 16)) & 0b00000111111000001111100000011111;
+    uint32_t result = ((((fg - bg) * alpha) >> 5) + bg) & 0b00000111111000001111100000011111;
+    return (uint16_t)((result >> 16) | result); // contract result
+}
